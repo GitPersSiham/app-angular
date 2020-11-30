c3fab72173419df81d2a546dfb3e36b0380641e2
GitPersSiham <lahouali.siham@outlook.fr>
2020-11-30 09:46:38 +0100

    intégration du router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {AppareilService} from "./services/appareil.service"
+import { Component } from '@angular/core';
+
 
 
 @Component({
@@ -7,35 +7,8 @@ import {AppareilService} from "./services/appareil.service"
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
+export class AppComponent  {
+constructor(){
 
-  appareils:any[];
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-  ngOnInit(){
-
-    this.appareils = this.appareilService.appareils
-  }
-
-  onAllumer(){
-  this.appareilService.switchOnAll()
-
- }
- onEteindre(){
-   this.appareilService.switchOfAll()
- }
+}
 }
