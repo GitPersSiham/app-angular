6601f2397218ab8513466826627f675f49f6eb07
GitPersSiham <lahouali.siham@outlook.fr>
2020-12-02 19:53:09 +0100

    ajout de l'objet subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable }from 'rxjs/Observable';
 import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Rx';
 
 
 @Component({
@@ -8,9 +9,10 @@ import 'rxjs/Rx';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy{
 
   secondes: number;
+  counterSubscription : Subscription
 
 constructor(){
 
@@ -18,16 +20,19 @@ constructor(){
 
 ngOnInit() {
  const counter = Observable.interval(1000);
- counter.subscribe(
-   (value: number) => {
-     this.secondes = value;
-   },
-   (error: any) => {
-    console.log('Une erreur a été rencontrée !')
-   },
-   () => {
-    console.log('observable complétée')
+ this.counterSubscription = counter.subscribe(
+   (value:number) => {
+   this.secondes = value;
+
    }
  );
 }
+ngOnDestroy(){
+ this.counterSubscription.unsubscribe();
+
+}
+
+
+
+
 }
